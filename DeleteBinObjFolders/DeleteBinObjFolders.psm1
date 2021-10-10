9ede55d27b392bb6accd33a3ff26ad41fbe18467
pmahend1 <prateek.mahendrakar@gmail.com>
2021-10-10 15:47:03 -0400

    Added Write-Progress with Job

--- DeleteBinObjFolders/DeleteBinObjFolders.psm1
+++ DeleteBinObjFolders/DeleteBinObjFolders.psm1
@@ -17,54 +17,59 @@
   If you want to exclude folders node_modules and temp from being deleted:
 
 #>
-function Remove-BinObjFolders 
-{
+function Remove-BinObjFolders {
     [CmdletBinding()]
     param 
     (
         [System.Array] $Exclude
     )
-    if ($Exclude.Length -eq 0) 
-    {
-        $binObjFolders = Get-ChildItem -Include bin, obj -Recurse -Force
+ 
+    $exclusionString = ''
+    if ($Exclude.Length -ne 0) {
+        $exclusionString = $Exclude | Join-String -Separator ',' 
     }
-    else 
-    {
-        $exclusions = $Exclude | Join-String -Separator ',' 
-        $binObjFolders = Get-ChildItem -Include bin, obj -Exclude $exclusions -Recurse -Force
+
+    $folderFindJob = Start-Job -ScriptBlock { Get-ChildItem -Include bin, obj -Exclude $exclusionString -Recurse -Force }
+
+    while ($folderFindJob.State -eq [System.Management.Automation.JobState]::Running) {
+        Write-Progress -Activity 'Remove-BinObjFolder' -Status "Searching for folder..."
     }
-    
-    if ($binObjFolders.Count -eq 0) 
-    {
+
+    $unsortedList = $folderFindJob | Wait-Job | Receive-Job
+
+    if ($unsortedList.Count -eq 0) {
         Write-Warning 'No bin, obj folders found!'
     }
-    else 
-    {
+    else {
         $yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes", "Will delete all bin, obj folders."
         $no = New-Object System.Management.Automation.Host.ChoiceDescription "&No", "Does NOT delete any bin, obj folders."
         $options = [System.Management.Automation.Host.ChoiceDescription[]]($yes, $no)
-    
+        
         $title = "Delete bin, obj folders" 
-        $message = "$($binObjFolders.Length) folders will be deleted. Do you want to continue?"
+        $message = "$($unsortedList.Length) folders will be deleted. Do you want to continue?"
         $result = $host.ui.PromptForChoice($title, $message, $options, 1)
-    
-        if ($result -eq 0) 
-        {
-            foreach ($item in $binObjFolders)
-            {
-                try 
-                {
+        
+        if ($result -eq 0) {
+
+            $sortedJob = Start-Job -ScriptBlock { $unsortedList | Select-Object FullName, @{Name = "FolderDepth"; Expression = { $_.DirectoryName.Split('\').Count } } | Sort-Object -Descending FolderDepth, FullName }
+
+            while ($sortedJob.State -eq [System.Management.Automation.JobState]::Running) {
+                Write-Progress -Activity "Remove-BinObjFolder" -status "Sorting folders..." 
+            }
+            
+            $sortedFolders = $sortedJob |  Wait-Job | Receive-Job
+            
+            foreach ($item in $sortedFolders) {
+                try {
                     Write-Host "Deleting " $item
                     Remove-Item $item -Recurse -Force
                 }
-                catch  [System.Exception] 
-                {
+                catch  [System.Exception] {
                     Write-Error $_
                 }
             }
         }
-        else 
-        {
+        else {
             Write-Warning "Selected No. Aborting..."
         }   
     }
